46e4954b039cd4abfe193ebe2ce7ac37dabc5487
fxpy <58055522+fxpy@users.noreply.github.com>
2021-05-31 22:27:52 +0300

    Create sherpa_install.sh
    init test ver

---
+++ sherpa_install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+#Install Rust and Sherpa Ceremony hash-gen
+
+sudo apt update && sudo apt upgrade -y
+sudo apt install pkg-config build-essential libssl-dev curl jq git -y
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+source $HOME/.cargo/env
+rustup update
+
+git clone https://github.com/Sherpa-Cash/phase2-bn254
+cd phase2-bn254/phase2
+
+cargo run --release --bin tornado
