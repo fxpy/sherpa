31d4fa1e917a2cdcee676e08fad8c7578f652ca6
fxpy <58055522+fxpy@users.noreply.github.com>
2021-05-31 22:32:30 +0300

    Update sherpa_install.sh
    add rust install automation

--- sherpa_install.sh
+++ sherpa_install.sh
@@ -5,7 +5,7 @@
 sudo apt update && sudo apt upgrade -y
 sudo apt install pkg-config build-essential libssl-dev curl jq git -y
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 source $HOME/.cargo/env
 rustup update
